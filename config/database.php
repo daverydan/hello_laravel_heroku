66621eb6fa8a6495b7bc97ba654f9badf16dda79
Danny Avery <danny.w.avery@gmail.com>
2016-08-31 09:47:18 -0600

    add DB variables

--- config/database.php
+++ config/database.php
@@ -2,6 +2,11 @@
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
